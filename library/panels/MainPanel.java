11a626209fda37c8417786bcc885de657bbdbcb1
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-15 20:34:57 +1000

    Final Test for tonight

--- library/panels/MainPanel.java
+++ library/panels/MainPanel.java
@@ -1,10 +1,10 @@
+/** Specifying the package in which the class resides */
 package library.panels;
 
+/** Import necessary libraries for the code */
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
-
 import library.interfaces.IMainListener;
-
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
@@ -12,10 +12,25 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 23.09.2016
+*@version 1.2
+*
+*	The class MainPanel is the program that creates a main panel object with a title, 
+* 	labels, and a button.
+*/
+
+/** Start of the class MainPanel */
 public class MainPanel extends JPanel {
 
+	/** Declare and initialise the variable that will be used in the class with private access modifiers */
 	private static final long serialVersionUID = 1L;
 
+	/** Constructor for a MainPanel object */
 	public MainPanel(IMainListener listener) {
 		setBorder(new TitledBorder(null, "Main Menu", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setBounds(500, 50, 470, 680);
@@ -44,4 +59,4 @@ public class MainPanel extends JPanel {
 		add(btnBorrowBooks);
         //setBounds(500, 100, 750, 615);
 	}
-}
+} // End of the class MainPanel
