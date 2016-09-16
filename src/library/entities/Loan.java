d71b99fe4e3b89476bb5ae60a1781d03e98241e5
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-16 11:57:35 +1000

    Author
    comments

--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -10,7 +10,7 @@ import library.interfaces.entities.IMember;
 import library.interfaces.entities.ELoanState;
 
 /**
-*@author Gourav Pathela
+*@author Chiranjivi Bashyal
 *@course Master of Information Technology
 *@subject Professional Programming Practice
 *@lecturer Dr Recep Ulusoy
