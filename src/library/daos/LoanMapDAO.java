b8629b2abb494b58c00a609ad0422650242bb318
gpathela <gpathela@yahoo.com>
2016-09-18 12:56:41 +1000

    updating method to find all loans by a member

--- src/library/daos/LoanMapDAO.java
+++ src/library/daos/LoanMapDAO.java
@@ -87,7 +87,7 @@ public class LoanMapDAO implements ILoanDAO {
 	}
 
 	@Override
-	public List<ILoan> findLoansByBorrower(IMember borrower) { 
+	public List<ILoan> findLoansByBorrower(IMember borrower) { // Method to find all loans by a member
 		if (borrower == null) {
 			throw new IllegalArgumentException(
 					String.format("LoanMapDAO : findLoansByBorrower : borrower cannot be null."));
