13c94fdd3d04e8e2fc6d17efa3f66ad58eaa856e
gpathela <gpathela@yahoo.com>
2016-09-18 12:33:18 +1000

    Added lecturer details

--- src/library/daos/LoanMapDAO.java
+++ src/library/daos/LoanMapDAO.java
@@ -19,10 +19,8 @@ import library.interfaces.entities.IMember;
 *@author Gourav Pathela
 *@course Master of Information Technology
 *@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
 
-*
-*		The LoanMapDAO class is the class that handle all loans. It check if loan is overdue, it find all loans by member,
-*		it finds all loans for the book etc. 
 */
 
 /** Start of the class LoanMapDAO */
