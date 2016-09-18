6b3ad8aa207dc305881e2f89b4c1b68019529c7a
gpathela <gpathela@yahoo.com>
2016-09-18 13:02:14 +1000

    Updating method to find ovedue loans

--- src/library/daos/LoanMapDAO.java
+++ src/library/daos/LoanMapDAO.java
@@ -126,7 +126,7 @@ public class LoanMapDAO implements ILoanDAO {
 	}
 
 	@Override
-	public List<ILoan> findOverDueLoans() { 
+	public List<ILoan> findOverDueLoans() { // Method to find all overdue loans
 		List<ILoan> list = new ArrayList<ILoan>();
 		for (ILoan loan : loanMap.values()) {
 			if (loan.isOverDue()) {
