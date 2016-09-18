685e3879bf29bb27fc55b0e896e441b12b4fd154
gpathela <gpathela@yahoo.com>
2016-09-18 12:48:56 +1000

    developing get loan method

--- src/library/daos/LoanMapDAO.java
+++ src/library/daos/LoanMapDAO.java
@@ -59,7 +59,7 @@ public class LoanMapDAO implements ILoanDAO {
 	}
 
 	@Override
-	public ILoan getLoanByID(int id) { // Method to get loan by Id
+	public ILoan getLoanByID(int id) { 
 		if (loanMap.containsKey(Integer.valueOf(id))) {
 			return loanMap.get(Integer.valueOf(id));
 		}
