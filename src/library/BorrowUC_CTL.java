964b7e3c622b55772d7775fda7524cdba449c875
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-19 22:06:13 +1000

    Day 3

--- src/library/BorrowUC_CTL.java
+++ src/library/BorrowUC_CTL.java
@@ -56,15 +56,12 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 	private List<ILoan> loanList;
 	private IMember borrower;
 
-
-	public BorrowUC_CTL(ICardReader reader, IScanner scanner, 
-			IPrinter printer, IDisplay display,
+	/** The constructor for a BorrowUC_CTL object which takes a parameter of variables */
+	public BorrowUC_CTL(ICardReader reader, IScanner scanner, IPrinter printer, IDisplay display,
 			IBookDAO bookDAO, ILoanDAO loanDAO, IMemberDAO memberDAO ) {
-
 		this.bookDAO = bookDAO;
 		this.memberDAO= memberDAO;
 		this.loanDAO = loanDAO;
-		
 		this.ui = new BorrowUC_UI(this);
 		this.reader = reader;
 		reader.addListener(this);
@@ -72,12 +69,13 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 		scanner.addListener(this);
 		this.printer = printer;
 		this.display = display;
-		this.display.setDisplay((JPanel) ui);
-		
+		this.display.setDisplay((JPanel) ui);	
 		setState(EBorrowState.INITIALIZED);
 	}
 
+	/** Override the cardSwiped method */
 	@Override
+	/** The method cardSwiped allows the user to swipe their card and displays the member's details */
 	public void cardSwiped(int memberID) {
 		System.out.println("cardSwiped: got " + memberID);
 		if (!state.equals(EBorrowState.INITIALIZED)) {
@@ -102,7 +100,7 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 			setState(EBorrowState.SCANNING_BOOKS);
 		}
 
-		//display member details
+		/** Display the details of the member */
 		int mID = borrower.getID();
 		String mName = borrower.getFirstName() + " " + borrower.getLastName();
 		String mContact = borrower.getContactPhone();
@@ -124,18 +122,18 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 			ui.displayOverFineLimitMessage(amountOwing);
 		}
 		
-		//display existing loans
+		/** Display existing loans */
 		for (ILoan ln : borrower.getLoans()) {
 			ui.displayExistingLoan(ln.toString());
 		}
 		
-		//initialize scanCount with number of existing loans
-		//so that member doesn't borrow more than they should
+		/** Initialise scanCount with number of existing loans so that member doesn't borrow more than they should */
 		scanCount = borrower.getLoans().size();
 	}
 	
 	
 	@Override
+	/** The method bookScanned sets the state of a book */
 	public void bookScanned(int barcode) {
 		System.out.println("bookScanned: got " + barcode);
 		if (state != EBorrowState.SCANNING_BOOKS) {
@@ -163,20 +161,18 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 		ILoan loan = loanDAO.createLoan(borrower, book);
 		loanList.add(loan);
 		
-		//display current book
+		/** Display current book */
 		ui.displayScannedBookDetails(book.toString());
-		//display pending loans
+		
+		/** Display pending loans */
 		ui.displayPendingLoan(loan.toString());
-
 		
 		if (scanCount >= MAX_COUNT) {
 			setState(EBorrowState.CONFIRMING_LOANS);
-		}
-		
-		
-		
+		}	
 	}
-
+	
+	/** The method setState sets the state of a book */
 	private void setState(EBorrowState state) {
 		System.out.println("Setting state: " + state);
 		
@@ -200,7 +196,7 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 		case CONFIRMING_LOANS:
 			reader.setEnabled(false);
 			scanner.setEnabled(false);
-			//display pending loans
+			/** display pending loans */
 			for (ILoan loan : loanList) {
 				ui.displayConfirmingLoan(loan.toString());
 			}
@@ -229,26 +225,33 @@ public class BorrowUC_CTL implements ICardReaderListener, IScannerListener, IBor
 			throw new RuntimeException("Unknown state");
 		}
 	}
-
+	
+	/** Override the cancelled method */
 	@Override
+	/** The method cancelled sets the state of the book to cancel */
 	public void cancelled() {
 		setState(EBorrowState.CANCELLED);		
 	}
 	
+	/** Override the scansCompleted method */
 	@Override
+	/** The method scansCompleted sets the state of borrow to scan complete */
 	public void scansCompleted() {
 		setState(EBorrowState.CONFIRMING_LOANS);		
 	}
 
+	/** Override the loansConfirmed method */
 	@Override
+	/** The method loansConfirmed sets the state of borrow to loan confirmed */
 	public void loansConfirmed() {
 		setState(EBorrowState.COMPLETED);				
 	}
 
+	/** Override the loansRejected method */
 	@Override
+	/** The method loansRejected sets the state of a loan to reject */
 	public void loansRejected() {
 		System.out.println("Loans Rejected");
 		setState(EBorrowState.SCANNING_BOOKS);		
-	}
-	
-}
+	}	
+} // End of the class BorrowUC_CTL
