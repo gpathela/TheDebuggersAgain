59e76a3ed7a5aba216c618c0b467b3c4db2a3a94
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 05:08:26 +1000

    Day 4

--- src/library/panels/CancelledPanel.java
+++ src/library/panels/CancelledPanel.java
@@ -1,17 +1,30 @@
+/** Import some Java libraries and specify the package in which the classes are organized */
 package library.panels;
 
+/** Import some libraries to be used in the code */
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The BorrowUC_UI 
+*/
+
+/** Start of the class CancelledPanel */
 public class CancelledPanel extends ABorrowPanel {
 
+	/** Declare and initialise a variable with private access modifiers */
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Create the panel.
-	 */
+	 /** The no args-constructor to create a CancelledPanel object  */
 	public CancelledPanel() {
 		setLayout(null);
 		setBorder(new TitledBorder(null, "Cancelled", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -23,6 +36,4 @@ public class CancelledPanel extends ABorrowPanel {
 		lblCancelled.setBounds(12, 187, 436, 78);
 		add(lblCancelled);		
 	}
-
-
-}
+} // End of the class CancelledPanel
