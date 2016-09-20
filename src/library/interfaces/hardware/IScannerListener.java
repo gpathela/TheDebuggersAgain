87603248c51d53680e08cff9ac7a4f28ea23f327
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 13:27:13 +1000

    Day 4

--- src/library/interfaces/hardware/IScannerListener.java
+++ src/library/interfaces/hardware/IScannerListener.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
-public interface IScannerListener {
-	
-	public void bookScanned(int barcode);
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IScannerListener contains one static method that enables the scan od a book.
+*/
 
+public interface IScannerListener {
+	/** Static method for the IScannerListener interface */	
+	public void bookScanned(int barcode);
 }
