6b5a4aebf8be36d2e14966f78a7f512ec73f7377
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 11:42:12 +1000

    Day 4

--- src/library/interfaces/IMainListener.java
+++ src/library/interfaces/IMainListener.java
@@ -1,7 +1,19 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
+
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IMainListener contains one static method that helps in borrowing a book
+*/
 
 public interface IMainListener {
 
+	/** Static method for the ILoanDAO interface */	
 	public void borrowBooks();
-
 }
