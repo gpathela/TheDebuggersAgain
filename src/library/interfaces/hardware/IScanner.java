36654d91fc0902765a588f321806f750c84a7082
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 13:17:24 +1000

    Day 4

--- src/library/interfaces/hardware/IScanner.java
+++ src/library/interfaces/hardware/IScanner.java
@@ -1,9 +1,20 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
-public interface IScanner {
-	
-	public void addListener(IScannerListener listener);
-	
-	public void setEnabled(boolean enabled);
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IScanner contains two static methods that enables the scanner to work and 
+*adds a listener to it.
+*/
 
+public interface IScanner {
+	/** Static method for the IMemberHelper interface */	
+	public void addListener(IScannerListener listener);
+	public void setEnabled(boolean enabled);
 }
