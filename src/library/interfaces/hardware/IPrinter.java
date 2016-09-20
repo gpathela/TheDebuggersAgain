c7a907c12f072282f510c9a36484387f260a2dc8
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 13:14:41 +1000

    Day 4

--- src/library/interfaces/hardware/IPrinter.java
+++ src/library/interfaces/hardware/IPrinter.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IPrinter contains a static method that helps print data.
+*/
+
 public interface IPrinter {
-	
-	public void print(String printData);
-	
+	/** Static method for the IMemberHelper interface */	
+	public void print(String printData);	
 }
