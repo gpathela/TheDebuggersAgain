e9155079fec3caf14aa97a179960176451214ba2
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 22:21:52 +1000

    Day 4

--- src/library/daos/LoanHelper.java
+++ src/library/daos/LoanHelper.java
@@ -1,8 +1,8 @@
-/** Import some Java libraries and specify the package in which the classes are organized */
+/** Specify the package in which the class resides */
 package library.daos;
 
+/** Import some libraries to be used in the code */
 import java.util.Date;
-
 import library.entities.Loan;
 import library.interfaces.daos.ILoanHelper;
 import library.interfaces.entities.IBook;
@@ -10,24 +10,22 @@ import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
 /**
-*@author Gourav Pathela
+*@reviewer Jean Claude Jino Rousseau
 *@course Master of Information Technology
 *@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy
-*@due date 23.09.2016
-*@version 1.0
-*
-*		The LoanHelper class is a class that implements ILoanHelper. 
-*		This class makes a object of ILoanHelper and returns new Loan.  
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
 */
 
-/** Start of the LoanHelper */
+/** Start of the class LoanHelper */
 public class LoanHelper implements ILoanHelper {
 
+	/** Override the makeLoan method */
 	@Override
+	/** The method makeLoan makes a loan and returns the new made loan */
 	public ILoan makeLoan(IBook book, IMember borrower, Date borrowDate, Date dueDate) {
 
 		return new Loan(book, borrower, borrowDate, dueDate);
 	}
-
-}
+} // End of the class LoanHelper
