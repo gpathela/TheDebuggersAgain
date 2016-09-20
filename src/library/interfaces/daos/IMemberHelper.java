198ff06f5784fbb855052493ae4a109d9cbb1c06
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 12:35:50 +1000

    Day 4

--- src/library/interfaces/daos/IMemberHelper.java
+++ src/library/interfaces/daos/IMemberHelper.java
@@ -1,9 +1,21 @@
+/** Specify the package in which the class resides */
 package library.interfaces.daos;
 
+/** Import some libraries to be used in the code */
 import library.interfaces.entities.IMember;
 
-public interface IMemberHelper {
-	
-	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id);
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IMemberHelper contains a static method that helps create a member.
+*/
 
+public interface IMemberHelper {
+	/** Static method for the IMemberHelper interface */	
+	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id);
 }
