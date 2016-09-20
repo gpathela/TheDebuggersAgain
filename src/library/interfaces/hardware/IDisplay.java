34c4ffeb07e47e40cf9b6c1e894589a61eebb9e5
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 11:28:18 +1000

    Day 4

--- src/library/interfaces/hardware/IDisplay.java
+++ src/library/interfaces/hardware/IDisplay.java
@@ -1,9 +1,21 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
+/** Import some libraries to be used in the code */
 import javax.swing.JPanel;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IDisplay contains one static method to enable the display of a panel
+*/
+
 public interface IDisplay {
-	
-	public void setDisplay(JPanel panel);
-	
+	/** Static method for the ICardReaderListener interface */
+	public void setDisplay(JPanel panel);	
 }
