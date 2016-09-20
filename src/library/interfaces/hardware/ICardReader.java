da37b75086d3e08459b9a06e2bd93089b2a4f70c
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 11:17:33 +1000

    Day 4

--- src/library/interfaces/hardware/ICardReader.java
+++ src/library/interfaces/hardware/ICardReader.java
@@ -1,9 +1,19 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface ICardReader contains several static method.
+*/
+
 public interface ICardReader {
-	
+	/** Static methods for the ICardReader interface */
 	public void addListener(ICardReaderListener listener);
-
 	public void setEnabled(boolean enabled);
-
 }
