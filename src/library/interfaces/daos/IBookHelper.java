b613204b7df4dff62ae6b8d38f6b4e11b9640c18
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 11:12:39 +1000

    Day 4

--- src/library/interfaces/daos/IBookHelper.java
+++ src/library/interfaces/daos/IBookHelper.java
@@ -1,9 +1,20 @@
+/** Specify the package in which the class resides */
 package library.interfaces.daos;
 
+/** Import some libraries to be used in the code */
 import library.interfaces.entities.IBook;
 
-public interface IBookHelper {
-	
-	public IBook makeBook(String author, String title, String callNumber, int id);
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IBookHelper contains one static method that creates a book of type IBook.
+*/
 
+public interface IBookHelper {
+	public IBook makeBook(String author, String title, String callNumber, int id);
 }
