039ad60491e869724075be0cc2c88ca2fa8a666a
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-20 11:15:52 +1000

    Day 4

--- src/library/interfaces/IBorrowUI.java
+++ src/library/interfaces/IBorrowUI.java
@@ -1,27 +1,28 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IBorrowUI contains several static method.
+*/
+
 public interface IBorrowUI {
-
+	/** Static methods for the IBorrowUI interface */
 	public void setState(EBorrowState state);
-	
 	public void displayMemberDetails(int memberID, String memberName, String memberPhone);
-
 	public void displayExistingLoan(String loanDetails);
-
 	public void displayOverDueMessage();
-	
 	public void displayAtLoanLimitMessage();
-	
 	public void displayOutstandingFineMessage(float amountOwing);
-
 	public void displayOverFineLimitMessage(float amountOwing);
-
 	public void displayScannedBookDetails(String bookDetails);
-
 	public void displayPendingLoan(String loanDetails);
-	
 	public void displayConfirmingLoan(String loanDetails);
-	
-	public void displayErrorMessage(String errorMesg);
-	
+	public void displayErrorMessage(String errorMesg);	
 }
--- src/library/interfaces/IBorrowUIListener.java
+++ src/library/interfaces/IBorrowUIListener.java
@@ -1,13 +1,21 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
 
-public interface IBorrowUIListener {
-	
-	public void cancelled();
-	
-	public void scansCompleted();
-	
-	public void loansConfirmed();
-	
-	public void loansRejected();
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The interface IBorrowUI contains several static method.
+*/
 
+public interface IBorrowUIListener {
+	/** Static methods for the IBorrowUI interface */
+	public void cancelled();
+	public void scansCompleted();
+	public void loansConfirmed();
+	public void loansRejected();
 }
