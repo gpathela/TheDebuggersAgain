5fc3f508dc56d6ef70fe578bd4480d4361546167
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-22 08:50:24 +1000

    Day 6

--- src/library/panels/ConfirmLoanPanel.java
+++ src/library/panels/ConfirmLoanPanel.java
@@ -16,7 +16,7 @@ import java.awt.Font;
 *@reviewer Jean Claude Jino Rousseau
 *@course Master of Information Technology
 *@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@lecturer Dr Recep Ulusoy
 *@due date 29.09.2016
 *@version 1.2
 *
