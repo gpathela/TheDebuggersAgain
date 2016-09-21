a34f476b314bb32ab7c19d8e6f6c627cee53a73f
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-21 11:18:12 +1000

    LoanMapDAO

--- src/library/daos/LoanMapDAO.java
+++ src/library/daos/LoanMapDAO.java
@@ -47,8 +47,7 @@ public class LoanMapDAO implements ILoanDAO {
 		cal = Calendar.getInstance();
 	}
 
-	public LoanMapDAO(ILoanHelper helper, Map<Integer, ILoan> loanMap) { // Public constructor that take object of ILoanHelper class and Map
-																			// objects of ILoan
+	public LoanMapDAO(ILoanHelper helper, Map<Integer, ILoan> loanMap) { // Public constructor that take object of ILoanHelper class and Map objects of ILoan
 		this(helper);
 		if (loanMap == null) {
 			throw new IllegalArgumentException(String.format("LoanMapDAO : constructor : loanMap cannot be null."));
