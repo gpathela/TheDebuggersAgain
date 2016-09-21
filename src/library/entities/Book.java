bbc578d1ea014c7a3e7981cbf1fee3f278a45bce
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 00:32:59 +1000

    book

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -4,7 +4,7 @@ import library.interfaces.entities.EBookState;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 
-public class Book implements IBook {
+public class Book implements IBook { //Book implements Ibook
 
 	
 	private String author;
