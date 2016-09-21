5af6ea31289cefa3eea7d7923f50986ac4f036ba
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-22 08:57:30 +1000

    Day 6

--- src/library/interfaces/entities/IMember.java
+++ src/library/interfaces/entities/IMember.java
@@ -8,7 +8,7 @@ import java.util.List;
 *@reviewer Jean Claude Jino Rousseau
 *@course Master of Information Technology
 *@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@lecturer Dr Recep Ulusoy 
 *@due date 29.09.2016
 *@version 1.2
 *
