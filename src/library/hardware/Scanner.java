932d1023a44f54c5fbe7084b5f713901a5836ef7
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-21 14:20:35 +1000

    Day 5

--- src/library/hardware/Scanner.java
+++ src/library/hardware/Scanner.java
@@ -1,31 +1,43 @@
+/** Specify the package in which the class resides */
 package library.hardware;
 
+/** Import some libraries to be used in the code */
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
-
 import library.interfaces.hardware.IScanner;
 import library.interfaces.hardware.IScannerListener;
-
 import javax.swing.JLabel;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The class Scanner 
+*/
+
+/** Start of the class Scanner */
 public class Scanner extends JFrame implements IScanner {
 
+	/** Declare and initialise variables with private access modifiers */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
 	private JButton btnScan;
 	private IScannerListener listener;
 
+	/** The no-args constructor for a Scanner object */
 	public Scanner() {
 		setTitle("Scanner");
         setBounds(50, 250, 400, 200);
@@ -84,21 +96,21 @@ public class Scanner extends JFrame implements IScanner {
 		btnScan.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnScan.setBounds(85, 88, 215, 39);
 		panel.add(btnScan);
-
 		//pack();
 	}
 
+	/** Override the setEnabled method */
 	@Override
+	/** The method setEnabled enables both the scan button and the text field */
 	public void setEnabled(boolean enabled) {
 		btnScan.setEnabled(enabled);
 		textField.setEditable(enabled);
-
 	}
 
+	/** Override the addListener method */
 	@Override
+	/** The method addListener adds a listener to the class */
 	public void addListener(IScannerListener listener) {
-		this.listener = listener;
-		
+		this.listener = listener;	
 	}
-
-}
+} // End of the class Scanner
