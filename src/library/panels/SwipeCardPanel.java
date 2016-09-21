8d672fed0e53f6a1de0b5ab56bc84556c62a8929
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-22 09:02:48 +1000

    Day 6

--- src/library/panels/SwipeCardPanel.java
+++ src/library/panels/SwipeCardPanel.java
@@ -17,7 +17,7 @@ import java.awt.Color;
 *@reviewer Jean Claude Jino Rousseau
 *@course Master of Information Technology
 *@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@lecturer Dr Recep Ulusoy
 *@due date 29.09.2016
 *@version 1.2
 *
