31e28d34ac2d45f2f1ed28d763e903bf02e0514d
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-22 08:54:30 +1000

    Day 6

--- src/library/interfaces/daos/IBookDAO.java
+++ src/library/interfaces/daos/IBookDAO.java
@@ -9,7 +9,7 @@ import library.interfaces.entities.IBook;
 *@reviewer Jean Claude Jino Rousseau
 *@course Master of Information Technology
 *@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@lecturer Dr Recep Ulusoy
 *@due date 29.09.2016
 *@version 1.2
 *
