fd40fda070e4dd6cefbad6954fd77033c01ef1c0
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-21 11:22:42 +1000

    Day 5

--- src/library/daos/MemberHelper.java
+++ src/library/daos/MemberHelper.java
@@ -1,16 +1,29 @@
+/** Specify the package in which the class resides */
 package library.daos;
 
+/** Import some libraries to be used in the code */
 import library.entities.Member;
 import library.interfaces.daos.IMemberHelper;
 import library.interfaces.entities.IMember;
 
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*
+*The class MemberHelper implements the IMemberHelper, creates and returns the Member object created.
+*/
+
+/** Start of the class MemberHelper */
 public class MemberHelper implements IMemberHelper {
 
+	/** Override the makeMember method */
 	@Override
-	public IMember makeMember(String firstName, String lastName,
-			String contactPhone, String emailAddress, int id) {
-		
+	/** The method makeMember creates and returns a Member object */
+	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id) {
 		return new Member(firstName, lastName, contactPhone, emailAddress, id);
 	}
-
-}
+} // End of the class MemberHelper
