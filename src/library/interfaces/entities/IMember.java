c55cb001c1b4a43782edff6da98dd45105263229
rpreet <rpreet72@gmail.com>
2016-09-22 16:01:06 +1000

    changes

--- src/library/interfaces/entities/IMember.java
+++ src/library/interfaces/entities/IMember.java
@@ -1,9 +1,19 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.entities;
 
 import java.util.List;
 
-public interface IMember {
-	
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IMember { //Start of IMember interface
+	/** Static methods for the IMember interface */
 	public static final int   LOAN_LIMIT = 5;
 	
 	public static final float FINE_LIMIT = 10.0f;	
@@ -42,4 +52,4 @@ public interface IMember {
 	public int     getID();
 	
 
-}
+}//End of IMember interface
