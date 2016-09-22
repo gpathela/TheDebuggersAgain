cfd7965535613d4dda66be367ad55c8607d717f2
rpreet <rpreet72@gmail.com>
2016-09-22 14:11:18 +1000

    changes

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -123,9 +123,10 @@ public class Book implements IBook {//the Book class implements IBook
 		}
 		state = EBookState.DISPOSED;	//set the book state to disposed
 	}
-
-	/** get method return the current book state */
+	
+	/** Override the getState method */
 	@Override
+	/** get method return the current book state */
 	public EBookState getState() {
 		return state;
 	}
