c71d5f9972f2ef4ed3ac72a91d4a47e821e3eba6
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 22:58:51 +1000

    ILoanDAO

--- src/library/interfaces/daos/ILoanDAO.java
+++ src/library/interfaces/daos/ILoanDAO.java
@@ -1,3 +1,4 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.daos;
 
 import java.util.Date;
@@ -7,8 +8,18 @@ import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
-public interface ILoanDAO {
-		
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface ILoanDAO {	//Start of ILoanDAO interface
+	
+	/** Static methods for the ILoanDAO interface */
 	public ILoan createLoan(IMember borrower, IBook book);
 
 	public void commitLoan(ILoan loan);
@@ -27,5 +38,4 @@ public interface ILoanDAO {
 
 	public List<ILoan> findOverDueLoans();
 
-}
-
+}	//End of ILoanDAO interface
