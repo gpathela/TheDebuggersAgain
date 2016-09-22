90714374d744ef830b95ebcd2921e957a7d7d887
rpreet <rpreet72@gmail.com>
2016-09-22 15:42:22 +1000

    changes

--- src/library/interfaces/daos/IBookHelper.java
+++ src/library/interfaces/daos/IBookHelper.java
@@ -1,9 +1,19 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.daos;
 
 import library.interfaces.entities.IBook;
 
-public interface IBookHelper {
-	
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IBookHelper {//Start of IBookHelper interface
+	/** Static method for IBookHelper interface */
 	public IBook makeBook(String author, String title, String callNumber, int id);
 
-}
+}	//End of IBookHelper interface
