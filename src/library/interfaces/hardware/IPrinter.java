b2650a78aa54f5d1e7f1c77ad0662851040a433d
rpreet <rpreet72@gmail.com>
2016-09-22 16:08:57 +1000

    changes

--- src/library/interfaces/hardware/IPrinter.java
+++ src/library/interfaces/hardware/IPrinter.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
-public interface IPrinter {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IPrinter { //Start of IPrinter interface
 	
+	/** Static methods for the IPrinter interface */
 	public void print(String printData);
 	
-}
+} //End of IPrinter interface
