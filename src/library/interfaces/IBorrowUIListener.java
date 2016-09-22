9e6ba7130b06effb4d3c50e312d8d4832929c899
rpreet <rpreet72@gmail.com>
2016-09-22 16:16:19 +1000

    changes

--- src/library/interfaces/IBorrowUIListener.java
+++ src/library/interfaces/IBorrowUIListener.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
 
-public interface IBorrowUIListener {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IBorrowUIListener { //Start of IBorrowUIListener interface
 	
+	/** Static methods for the IBorrowUIListener interface */
 	public void cancelled();
 	
 	public void scansCompleted();
@@ -10,4 +21,4 @@ public interface IBorrowUIListener {
 	
 	public void loansRejected();
 
-}
+} //End of IBorrowUIListener interface
