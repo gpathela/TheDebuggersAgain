cafdef6982d8bfa384f81a5b7fc38ab66b824400
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 21:50:06 +1000

    Display

--- src/library/hardware/Display.java
+++ src/library/hardware/Display.java
@@ -1,3 +1,4 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.hardware;
 
 import javax.swing.JFrame;
@@ -5,10 +6,23 @@ import javax.swing.JPanel;
 
 import library.interfaces.hardware.IDisplay;
 
-public class Display extends JFrame implements IDisplay {
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
 
+/** Start of the class Display */
+public class Display extends JFrame implements IDisplay {
+	
+	/** Declare and initialize the variable for the class
+		with private visibility */
 	private static final long serialVersionUID = 1L;
 	
+	/** The no args-constructor(default constructor) to create a Display object */ 
 	public Display() {
 		setTitle("Display");
 		setBounds(500, 50, 470, 680);
@@ -16,10 +30,10 @@ public class Display extends JFrame implements IDisplay {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 	}
 
-
+	/** Override the setDisplay method */
 	@Override
 	public void setDisplay(JPanel panel) {
         getContentPane().add(panel, null);		
 	}
 	
-}
+}// End of the class Display
