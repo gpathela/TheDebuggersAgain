d8218e269831d1363ebcad99cbc6dcfb505ce280
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 23:59:23 +1000

    changes

--- src/library/entities/TestLoan.java
+++ src/library/entities/TestLoan.java
@@ -1,4 +1,4 @@
-/** Import some Java libraries and specify the package in which the classes are organized */
+/** Import some Java libraries and specify the package in which the classes are organized. */
 package library.entities;
 
 import java.util.Calendar;
