41920cb13c3ea54058bd3a50f499d049f79fd953
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 23:01:06 +1000

    IMemberHelper

--- src/library/interfaces/daos/IMemberHelper.java
+++ src/library/interfaces/daos/IMemberHelper.java
@@ -1,9 +1,19 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.daos;
 
 import library.interfaces.entities.IMember;
 
-public interface IMemberHelper {
-	
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IMemberHelper {	//Start of IMemberHelper interface
+	/** Static methods for the IMemberHelper interface */
 	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id);
 
-}
+}//End of IMemberHelper interface
