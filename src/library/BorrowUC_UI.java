bd752b113d4903d6136c7f37a268c2ccb8467e2b
rpreet <rpreet72@gmail.com>
2016-09-22 20:10:18 +1000

    changes

--- src/library/BorrowUC_UI.java
+++ src/library/BorrowUC_UI.java
@@ -1,3 +1,4 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library;
 
 import java.awt.CardLayout;
@@ -15,15 +16,27 @@ import library.panels.SwipeCardPanel;
 import library.panels.ConfirmLoanPanel;
 import library.panels.ScanningPanel;
 
-public class BorrowUC_UI extends ABorrowPanel {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.2
+*/
 
+/** Start of the class BorrowUC_UI */
+public class BorrowUC_UI extends ABorrowPanel {
+	
+	/** Declare and initialize the variable for the class
+		with private visibility */
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private IBorrowUIListener listener;
 	private EBorrowState state;
 	private Map<EBorrowState,IBorrowUI> panels;
 
-	
+	/** Construct an BorrowUC_UI object with the specified attribute. */
 	public BorrowUC_UI(IBorrowUIListener listener) {
 		this.listener = listener;
 		this.panels = new HashMap<EBorrowState,IBorrowUI>();		
@@ -37,12 +50,13 @@ public class BorrowUC_UI extends ABorrowPanel {
 		addPanel(new CompletedPanel(),           EBorrowState.COMPLETED);
 	}
 	
+	/** The addPanel method adds panel to the screen */
 	private void addPanel(ABorrowPanel panel, EBorrowState state) {
         this.panels.put(state, panel);
         this.add(panel, state.toString());
  	}
 
-
+	/** Override the setState method to sets the state of EBorrow*/
 	@Override
 	public void setState(EBorrowState state) {
 		CardLayout cl = (CardLayout) (this.getLayout());
@@ -72,67 +86,68 @@ public class BorrowUC_UI extends ABorrowPanel {
 		this.state = state;
 	}
 
-
+	/** Override the displayMemberDetails method to displays the details of a member */
 	@Override
 	public void displayMemberDetails(int memberID, String memberName, String memberPhone) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayMemberDetails( memberID,  memberName, memberPhone);		
 	}
 
-
+	/** Override the displayOverDueMessage method to displays any overdue message to the user*/
 	@Override
 	public void displayOverDueMessage() {
 		IBorrowUI ui = panels.get(state);
 		ui.displayOverDueMessage();		
 	}
 
-
+	/** Override the displayAtLoanLimitMessage method */
 	@Override
 	public void displayAtLoanLimitMessage() {
 		IBorrowUI ui = panels.get(state);
 		ui.displayAtLoanLimitMessage();		
 	}
 
-
+	
+	/** Override the displayOutstandingFineMessage method to displays a message of any outstanding to the user */
 	@Override
 	public void displayOutstandingFineMessage(float amountOwing) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayOutstandingFineMessage(amountOwing);		
 	}
 
-	
+	/** Override the displayExistingLoan method to displays the details of an existing loan to the user */
 	@Override
 	public void displayExistingLoan(String loanDetails) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayExistingLoan(loanDetails);		
 	}
 
-	
+	/** Override the displayScannedBookDetails method show the details of a scanned book to the screen */
 	@Override
 	public void displayScannedBookDetails(String bookDetails) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayScannedBookDetails(bookDetails);		
 	}
 
-	
+	/** Override the displayPendingLoan method displays a pending loan to the screen*/
 	@Override
 	public void displayPendingLoan(String loanDetails) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayPendingLoan(loanDetails);		
 	}
 
-	
+	/** Override the displayConfirmingLoan method displays a confirming loan message to the user*/
 	@Override
 	public void displayConfirmingLoan(String loanDetails) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayConfirmingLoan(loanDetails);		
 	}
 
-	
+	/** Override the displayErrorMessage method displays an error message to the screen*/
 	@Override
 	public void displayErrorMessage(String errorMesg) {
 		IBorrowUI ui = panels.get(state);
 		ui.displayErrorMessage(errorMesg);		
 	}
 
-}
+} // End of the class BorrowUC_UI
