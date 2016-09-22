3f07c683e7c6a0e0fdebb100cd92964a5f5379c4
rpreet <rpreet72@gmail.com>
2016-09-22 16:14:57 +1000

    changes

--- src/library/interfaces/IBorrowUI.java
+++ src/library/interfaces/IBorrowUI.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
 
-public interface IBorrowUI {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
 
+public interface IBorrowUI { //Start of IBorrowUI interface
+	
+	/** Static methods for the IBorrowUI interface */
 	public void setState(EBorrowState state);
 	
 	public void displayMemberDetails(int memberID, String memberName, String memberPhone);
@@ -24,4 +35,4 @@ public interface IBorrowUI {
 	
 	public void displayErrorMessage(String errorMesg);
 	
-}
+}  //End of IBorrowUI interface
