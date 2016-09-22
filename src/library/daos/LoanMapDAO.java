55976d63c88a426b98ac842f3c4dc7641c81dc24
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 21:40:56 +1000

    LoanMapDAO

--- src/library/daos/LoanMapDAO.java
+++ src/library/daos/LoanMapDAO.java
@@ -1,6 +1,7 @@
-/** Import some Java libraries and specify the package in which the classes are organized*/
+/** Specify the package in which the class resides */
 package library.daos;
 
+/** Import some libraries to be used in the code */
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -16,30 +17,31 @@ import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
 /**
-*@reviewer Chiranjivi Bashyal
-*@course Master of Information Technology
-*@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy
-*@due date 23.09.2016
-*@version 1.0
-*/
-
-/**
-*		The LoanMapDAO class is the class that handle all loans. It check if loan is overdue, it find all loans by member, it finds all loans for the book etc. 
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.0
+* The LoanMapDAO class is the class that handle all loans. 
+*
 */
 
 /** Start of the class LoanMapDAO */
-public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
+public class LoanMapDAO implements ILoanDAO {
 
-	/**
-	 * Declare the variable for the class with private visibility modifiers
-	 */
+	/** 
+	* Declare and initialize the variable for the class
+	* with private visibility 
+	*/
 	private int nextID;
 	private Map<Integer, ILoan> loanMap;
 	private ILoanHelper helper;
 	private Calendar cal;
-
-	public LoanMapDAO(ILoanHelper helper) { // Public constructor that take object of ILoanHelper class
+	
+	/** Construct an LoanMapDAO object with the specified attribute. */
+	public LoanMapDAO(ILoanHelper helper) { // Public constructor that take
+											// object of ILoanHelper class
 		if (helper == null) {
 			throw new IllegalArgumentException(String.format("LoanMapDAO : constructor : helper cannot be null."));
 		}
@@ -48,23 +50,27 @@ public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
 		loanMap = new HashMap<Integer, ILoan>();
 		cal = Calendar.getInstance();
 	}
-
-	public LoanMapDAO(ILoanHelper helper, Map<Integer, ILoan> loanMap) { // Public constructor that take object of ILoanHelper class and Map objects of ILoan
+	
+	/** The public constructor LoanMapDAO takes an object of ILoanHelper class and Map objects of ILoan */
+	public LoanMapDAO(ILoanHelper helper, Map<Integer, ILoan> loanMap) { 
 		this(helper);
 		if (loanMap == null) {
 			throw new IllegalArgumentException(String.format("LoanMapDAO : constructor : loanMap cannot be null."));
 		}
 		this.loanMap = loanMap;
 	}
-
+	
+	/** Override the getLoanByID method */
 	@Override
+	/** The getLoanByID method returns a loan by using ID */
 	public ILoan getLoanByID(int id) {	//Method to get loan by Id
 		if (loanMap.containsKey(Integer.valueOf(id))) {
 			return loanMap.get(Integer.valueOf(id));
 		}
 		return null;
 	}
-
+	
+	/** Override the getLoanByBook method */
 	@Override
 	public ILoan getLoanByBook(IBook book) {	//Method to get loan by book
 		if (book == null) {
@@ -78,14 +84,18 @@ public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
 		}
 		return null;
 	}
-
+	
+	/** Override the listLoans method */
 	@Override
+	/** The listLoans method creates list of all loans */
 	public List<ILoan> listLoans() {	//Method to generate list of all loans
 		List<ILoan> list = new ArrayList<ILoan>(loanMap.values());
 		return Collections.unmodifiableList(list);
 	}
-
+	
+	/** Override the findLoansByBorrower method */
 	@Override
+	/** The findLoansByBorrower method returns a list of loans by borrower */
 	public List<ILoan> findLoansByBorrower(IMember borrower) {	//Method to find all loans by a member
 		if (borrower == null) {
 			throw new IllegalArgumentException(
@@ -99,8 +109,10 @@ public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
 		}
 		return Collections.unmodifiableList(list);
 	}
-
+	
+	/** Override the findLoansByBookTitle method */
 	@Override
+	/** The findLoansByBookTitle method returns a list of loans by book title */
 	public List<ILoan> findLoansByBookTitle(String title) {	//Method to find loans on book title
 		if (title == null || title.isEmpty()) {
 			throw new IllegalArgumentException(
@@ -115,15 +127,19 @@ public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
 		}
 		return Collections.unmodifiableList(list);
 	}
-
+	
+	/** Override the updateOverDueStatus method */
 	@Override
+	/** The updateOverDueStatus method updates the status of a loan*/
 	public void updateOverDueStatus(Date currentDate) {	//Method to update status of a loan
 		for (ILoan loan : loanMap.values()) {
 			loan.checkOverDue(currentDate);
 		}
 	}
-
+	
+	/** Override the findOverDueLoans method */
 	@Override
+	/** The findOverDueLoans method returns a collection of overdue loans */
 	public List<ILoan> findOverDueLoans() {	//Method to find all overdue loans
 		List<ILoan> list = new ArrayList<ILoan>();
 		for (ILoan loan : loanMap.values()) {
@@ -133,12 +149,15 @@ public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
 		}
 		return Collections.unmodifiableList(list);
 	}
-
+	
+	/** The getNextId method returns the next index of a loan */
 	private int getNextId() {
 		return ++nextID;
 	}
-
+	
+	/** Override the createLoan method */
 	@Override
+	/** The createLoan method returns a created loan based on a book and borrower objects*/
 	public ILoan createLoan(IMember borrower, IBook book) {	//Method to create loan
 		Date borrowDate = new Date();
 		cal.setTime(borrowDate);
@@ -147,12 +166,14 @@ public class LoanMapDAO implements ILoanDAO { //LoanMapDAO implements IMapDAO
 		ILoan loan = helper.makeLoan(book, borrower, borrowDate, dueDate);
 		return loan;
 	}
-
+	
+	/** Override the commitLoan method */
 	@Override
+	/** The commitLoan method commits a loan to a loan map */
 	public void commitLoan(ILoan loan) {	//Method to commit loan to loan Map
 		int id = getNextId();
 		loan.commit(id);
 		loanMap.put(id, loan);
 	}
 
-}
+}// End of the class LoanMapDAO
