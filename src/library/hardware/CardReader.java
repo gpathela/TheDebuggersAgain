f207bb8a64c3175cf0bcf4357e95149527b7aa71
rpreet <rpreet72@gmail.com>
2016-09-22 15:15:03 +1000

    changes

--- src/library/hardware/CardReader.java
+++ src/library/hardware/CardReader.java
@@ -106,7 +106,7 @@ public class CardReader extends JFrame implements ICardReader {
 		textField.setEditable(enabled);
 	}
 
-
+	/** Override the addListener method */
 	@Override
 	public void addListener(ICardReaderListener listener) {
 		this.listener = listener;		
