e8ea841ee03ae4a2ae3231283e5ce67961f2a3b9
rpreet <rpreet72@gmail.com>
2016-09-22 16:13:22 +1000

    changes

--- src/library/interfaces/EBorrowState.java
+++ src/library/interfaces/EBorrowState.java
@@ -1,6 +1,15 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
 
-public enum EBorrowState {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+public enum EBorrowState { //Start of EBorrowState enumeration class
 	CREATED, INITIALIZED, SCANNING_BOOKS, CONFIRMING_LOANS, COMPLETED, BORROWING_RESTRICTED, CANCELLED 
 
-}
+} //End of EBorrowState enumeration class
