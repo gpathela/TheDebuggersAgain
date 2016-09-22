d65b0d0d98e17b44fb2683482bcd656d9e223b3a
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 21:51:51 +1000

    Scanner

--- src/library/hardware/Scanner.java
+++ src/library/hardware/Scanner.java
@@ -1,3 +1,4 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.hardware;
 
 import javax.swing.JFrame;
@@ -19,13 +20,26 @@ import java.awt.event.KeyEvent;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+/** Start of the class Scanner */
 public class Scanner extends JFrame implements IScanner {
 
+	/** Declare and initialize the variable for the class
+		with private visibility */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
 	private JButton btnScan;
 	private IScannerListener listener;
-
+	
+	/** The no args-constructor(default constructor) to create a Scanner object */
 	public Scanner() {
 		setTitle("Scanner");
         setBounds(50, 250, 400, 200);
@@ -88,17 +102,19 @@ public class Scanner extends JFrame implements IScanner {
 		//pack();
 	}
 
+	/** Override the setEnabled method helps to enables both the scan button and the text field*/
 	@Override
 	public void setEnabled(boolean enabled) {
 		btnScan.setEnabled(enabled);
 		textField.setEditable(enabled);
 
 	}
-
+	
+	/** Override the addListener method to adds a listener to the class */
 	@Override
 	public void addListener(IScannerListener listener) {
 		this.listener = listener;
 		
 	}
 
-}
+}// End of the class Scanner
