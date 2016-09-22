80905b71a02bd09e2c7d23bcab47703da1041cef
rpreet <rpreet72@gmail.com>
2016-09-22 13:13:54 +1000

    changes

--- src/library/daos/MemberHelper.java
+++ src/library/daos/MemberHelper.java
@@ -5,6 +5,15 @@ import library.entities.Member;
 import library.interfaces.daos.IMemberHelper;
 import library.interfaces.entities.IMember;
 
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.0
+*/
+
 public class MemberHelper implements IMemberHelper {
 
 	@Override
