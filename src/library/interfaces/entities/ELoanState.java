2c42aebb949c14677b0649e4570e051406a16d81
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 23:04:21 +1000

    ELoanState

--- src/library/interfaces/entities/ELoanState.java
+++ src/library/interfaces/entities/ELoanState.java
@@ -1,7 +1,17 @@
+/** Specify the package in which the class resides */
 package library.interfaces.entities;
 
-public enum ELoanState {
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public enum ELoanState {	//Start of ELoanState enumeration class
 	
 	PENDING, CURRENT, OVERDUE, COMPLETE
 
-}
+} //End of ELoanState enumeration class
