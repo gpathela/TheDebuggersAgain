a7b6dc98a2e4cdfffc1a7eea24694eac0391a661
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 23:34:43 +1000

    IScanner

--- src/library/interfaces/hardware/IScanner.java
+++ src/library/interfaces/hardware/IScanner.java
@@ -1,9 +1,20 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
-public interface IScanner {
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IScanner { //Start of IScanner interface
 	
+	/** Static methods for the IScanner interface */
 	public void addListener(IScannerListener listener);
 	
 	public void setEnabled(boolean enabled);
 
-}
+} //End of IScanner interface
