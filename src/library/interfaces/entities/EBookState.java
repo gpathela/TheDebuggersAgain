edf2f1b6ec940a26054851778d31602dfdcba55d
rpreet <rpreet72@gmail.com>
2016-09-22 15:52:47 +1000

    changes

--- src/library/interfaces/entities/EBookState.java
+++ src/library/interfaces/entities/EBookState.java
@@ -1,6 +1,15 @@
+/** Specify the package in which the class resides */
 package library.interfaces.entities;
 
-public enum EBookState {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+public enum EBookState {//Start of EBookState enumeration class
 	AVAILABLE, ON_LOAN, LOST, DAMAGED, DISPOSED 
 
-}
+} //End of EBookState enumeration class
