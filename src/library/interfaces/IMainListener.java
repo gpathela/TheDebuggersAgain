3ce2b1a50997774240df9ede66bec74a5d31ff80
rpreet <rpreet72@gmail.com>
2016-09-22 16:17:45 +1000

    changes

--- src/library/interfaces/IMainListener.java
+++ src/library/interfaces/IMainListener.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces;
 
-public interface IMainListener {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
 
+public interface IMainListener { //Start of IMainListener interface
+	
+	/** Static methods for the IMainListener interface */
 	public void borrowBooks();
 
-}
+} //End of IMainListener interface
