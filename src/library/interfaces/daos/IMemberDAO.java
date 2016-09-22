0b84c442cacdff579589a8dcd0a9eee73ce6f4d0
rpreet <rpreet72@gmail.com>
2016-09-22 15:48:45 +1000

    changes

--- src/library/interfaces/daos/IMemberDAO.java
+++ src/library/interfaces/daos/IMemberDAO.java
@@ -1,11 +1,22 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.daos;
 
 import java.util.List;
 
 import library.interfaces.entities.IMember;
 
-public interface IMemberDAO {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
 
+public interface IMemberDAO {//Start of IMemberDAO interface
+		
+		/** Static methods for the IMemberDAO interface */
 		public IMember addMember(String firstName, String lastName, String ContactPhone, String emailAddress);
 		
 		public IMember getMemberByID(int id);
@@ -18,4 +29,4 @@ public interface IMemberDAO {
 		
 		public List<IMember> findMembersByNames(String firstName, String lastName);
 
-}
+}//End of IMemberDAO interface
