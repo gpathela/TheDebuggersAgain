13138867fe8a1d327f818386a20848cf3ef1fa56
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 21:34:46 +1000

    BookHelper

--- src/library/daos/BookHelper.java
+++ src/library/daos/BookHelper.java
@@ -1,34 +1,31 @@
-/** Import some Java libraries and specify the package in which the classes are organized */
+/** Specify the package in which this code resides */
 package library.daos;
+
+/** Import some libraries to be used in the code */
 import library.entities.Book;
-
-
-/** Import some libraries */
 import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
-
 /**
-*@reviewer Chiranjivi Bashyal
-*@course Master of Information Technology
-*@subject Professional Programming Practice
-*@lecturer Dr Recep Ulusoy
-*@due date 23.09.2016
-*@version 1.0
+* @author Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.0
+*
+* The BookHelper class implements the IBookHelper interface. 
+* This class shows the details of the book.
+*
 */
 
-/** The BookHelper class implements the interface IBookHelper. It displays the details about a 
-*book that it creates.
-*/
-/** start of class BookHelper */
-
+/** Start of the class BookHelper */
 public class BookHelper implements IBookHelper {
 
 	@Override
-	
-	/** The method makeBook creates a book and returns the details of the created book object */
+	/** The makeBook method creates the book and return its details*/
 	public IBook makeBook(String author, String title, String callNumber, int id) {
 		return new Book(author, title, callNumber, id);		
 	}
 
-} // End of class BookHelper
+}// End of the class BookHelper
