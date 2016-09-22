1e60e438f068d94c9d4865543e09f04cad16623e
rpreet <rpreet72@gmail.com>
2016-09-22 12:12:20 +1000

    some changes

--- src/library/daos/BookHelper.java
+++ src/library/daos/BookHelper.java
@@ -1,6 +1,7 @@
 /** Specify the package in which this code resides */
 package library.daos;
 
+/** Import some libraries to be used in the code */
 import library.entities.Book;
 import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
