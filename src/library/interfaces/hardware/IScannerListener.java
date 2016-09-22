8b98742208d64a1f4e953f05c87a96c328b365c4
rpreet <rpreet72@gmail.com>
2016-09-22 16:11:38 +1000

    changes

--- src/library/interfaces/hardware/IScannerListener.java
+++ src/library/interfaces/hardware/IScannerListener.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
-public interface IScannerListener {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IScannerListener { //Start of IScannerListener interface
 	
+	/** Static methods for the IScannerListener interface */
 	public void bookScanned(int barcode);
 
-}
+} //End of IScannerListener interface
