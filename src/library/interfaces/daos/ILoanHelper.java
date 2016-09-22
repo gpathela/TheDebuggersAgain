e0740e279ad9e05a7891c5f50f16749868199160
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-22 22:59:33 +1000

    ILoanHelper

--- src/library/interfaces/daos/ILoanHelper.java
+++ src/library/interfaces/daos/ILoanHelper.java
@@ -1,3 +1,4 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.daos;
 
 import java.util.Date;
@@ -6,8 +7,18 @@ import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
-public interface ILoanHelper {
+/**
+* @reviewer Chiranjivi Bashyal
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface ILoanHelper {	//Start of ILoanHelper interface
 	
+	/** Static methods for the ILoanHelper interface */
 	public ILoan makeLoan(IBook book, IMember borrower, Date borrowDate, Date dueDate);
 
-}
+}//End of ILoanHelper interface
