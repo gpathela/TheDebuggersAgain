00732b5132de64e75f97894e08546906d14b5661
rpreet <rpreet72@gmail.com>
2016-09-22 15:39:52 +1000

    changes

--- src/library/interfaces/daos/IBookDAO.java
+++ src/library/interfaces/daos/IBookDAO.java
@@ -1,11 +1,22 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.daos;
 
 import java.util.List;
 
 import library.interfaces.entities.IBook;
 
-public interface IBookDAO {
-	
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+
+public interface IBookDAO {	//Start of IBookDAO interface
+	/** Static methods for the IBookDAO interface */
 	public IBook addBook(String author, String title, String callNo);
 	
 	public IBook getBookByID(int id);
@@ -18,4 +29,4 @@ public interface IBookDAO {
 	
 	public List<IBook> findBooksByAuthorTitle(String author, String title);
 
-}
+}	//End of IBookDAO interface
