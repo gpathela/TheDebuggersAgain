34fefec781d1de514bcb594cd2e680bb98a52edf
rpreet <rpreet72@gmail.com>
2016-09-22 16:07:22 +1000

    changes

--- src/library/interfaces/hardware/IDisplay.java
+++ src/library/interfaces/hardware/IDisplay.java
@@ -1,9 +1,20 @@
+/** Import some Java libraries and specify the package in which the classes resides */
 package library.interfaces.hardware;
 
 import javax.swing.JPanel;
 
-public interface IDisplay {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface IDisplay { //Start of IDisplay interface
 	
+	/** Static methods for the IDisplay interface */
 	public void setDisplay(JPanel panel);
 	
-}
+} //End of IDisplay interface
