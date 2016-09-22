b0cfdd8d9f54c5735dbce144ea33578f5ed70385
rpreet <rpreet72@gmail.com>
2016-09-22 16:05:42 +1000

    changes

--- src/library/interfaces/hardware/ICardReaderListener.java
+++ src/library/interfaces/hardware/ICardReaderListener.java
@@ -1,7 +1,18 @@
+/** Specify the package in which the class resides */
 package library.interfaces.hardware;
 
-public interface ICardReaderListener {
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+public interface ICardReaderListener {	//Start of ICardReaderListener interface
 	
+	/** Static methods for the ICardReaderListener interface */
 	public void cardSwiped(int cardData);
 
-}
+}//End of ICardReaderListener interface
