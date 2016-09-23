45151b602cf52c36cd16f9cf541c8983dfaef2f8
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-09-24 09:04:00 +1000

    Refining the TestGetAuthor class

--- Book Unit Test/TestGetAuthor.java
+++ Book Unit Test/TestGetAuthor.java
@@ -1,13 +1,27 @@
+/**
+*@Author Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 29.09.2016
+*@version 1.2
+*
+*The TestGetAuthor class is a program that tests the method that returns the author of a book.
+*/
 public class TestGetAuthor {
 	public static void main(String[] args) {
+		/** Create a book with a functional parameter */
 		Book b0 = new Book("Recep", "Harry Potter", "R123", 111);
+		
+		/** Create a book with a non-functional parameter */
 		Book b1 = new Book(1234, "Boom", "B000", 222);
-		System.out.println("\nTesting the getAuthor() method from the book class....");
 		
 		/** Display the results */
-		System.out.println("The author of the created book is:");
+		System.out.println("\nTesting the getAuthor() method from the book class....");
+		System.out.println("The author of book b0 is:");
 		System.out.println(b0.getAuthor());
-		System.out.println("The author of the created book is going to fail cos of int value instead of a string");
+		System.out.println("The program will not be able to display the author for book b1.");
+		System.out.println("The reason is that an integer has been inserted instead of a string for the author name.");
 		System.out.println(b1.getAuthor());
 	}
 }
