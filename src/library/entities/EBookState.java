182eae98134ad11b527a0046ada7d4023564e998
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-23 14:48:02 +1000

    EBookState

---
+++ src/library/entities/EBookState.java
@@ -0,0 +1,16 @@
+/** Specify the package in which the class resides */
+package library.interfaces.entities;
+
+/**
+*@reviewer Chiranjivi Bashyal
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*/
+
+/** Start of the enumeration class which holds a collection of constants */
+public enum EBookState {
+	AVAILABLE, ON_LOAN, LOST, DAMAGED, DISPOSED 
+}
