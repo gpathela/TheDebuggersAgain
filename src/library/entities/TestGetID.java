623049eaa876cee6519b372a2edfa7da1147b160
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-23 14:51:17 +1000

    testing GetID

---
+++ src/library/entities/TestGetID.java
@@ -0,0 +1,13 @@
+public class TestGetID{
+	public static void main(String[] args) {
+		Book b0 = new Book("Bikash", "Nepali", "A123", 222);
+		Book b1 = new Book("Bikash", "Nepali", "R123", abc);
+		System.out.println("\nTesting the getID() method from the book class....");
+		
+		/** Display the results */
+		System.out.println("The ID of the created book is:");
+		System.out.println(b0.getID());
+		System.out.println("The ID of the created book is:");
+		System.out.println(b1.getID());
+	}
+}
