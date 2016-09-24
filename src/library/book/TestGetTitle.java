329864d1c13def773a97b225929bfc87e3242f20
rpreet <rpreet72@gmail.com>
2016-09-24 15:15:30 +1000

    Testing the title of the book

--- src/library/book/TestGetTitle.java
+++ src/library/book/TestGetTitle.java
@@ -1,13 +1,25 @@
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+/** Start of class TestGetTitle */
 public class TestGetTitle {
-	public static void main(String[] args) {
-		Book b0 = new Book("Recep", "Harry Poter", "R123", 111);
-		//Book b1 = new Book(Joo, " 1524", "R123", 111);
-		System.out.println("\nTesting the getTitle() method from the book class....");
+	public static void main(String[] args) {	//Start of main method
+		Book b0 = new Book("Nelson", "Murder In Paris", "R123", 111);
+		Book b1 = new Book("Nelson", 1234, "R123", 111);
+		System.out.println("\nTesting the getTitle() method from the book class");
+		System.out.println("******************************************************");
+		System.out.println("");
 		
 		/** Display the results */
-		System.out.println("The title of the created book is:");
-		System.out.println(b0.getTitle());
-		//System.out.println("The title of the created book is going to fail cos of int value:");
-		//System.out.println(b1.getTitle());
-	}
-}
+		System.out.println("The title of the created book is: " + b0.getTitle());
+		
+		System.out.println("The title of the created book is going to fail cos of int value: "+ b1.getTitle());
+		
+	}	//End of main method
+}	//End of TestGetTitle
