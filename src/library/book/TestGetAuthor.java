987885fb292eedf02d4e66ad9bf7c3dd43aa08d7
rpreet <rpreet72@gmail.com>
2016-09-24 14:51:00 +1000

    testing Author of the book

--- src/library/book/TestGetAuthor.java
+++ src/library/book/TestGetAuthor.java
@@ -1,13 +1,24 @@
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+/** Start of class TestGetAuthor */
 public class TestGetAuthor {
-	public static void main(String[] args) {
-		Book b0 = new Book("Recep", "Harry Poter", "R123", 111);
-		Book b1 = new Book(1234, "Harry Poter", "R123", 111);
-		System.out.println("\nTesting the getAuthor() method from the book class....");
+	public static void main(String[] args) {	//Start of main method
+		Book b0 = new Book("Nelson", "Murder In Paris", "R123", 111);
+		Book b1 = new Book(1234, "Murder In Paris", "R123", 111);
+		System.out.println("\nTesting the getAuthor() method from the book class");
+		System.out.println("******************************************************");
+		System.out.println("");
 		
 		/** Display the results */
-		System.out.println("The author of the created book is:");
-		System.out.println(b0.getAuthor());
-		System.out.println("The author of the created book is going to fail cos of int value:");
-		System.out.println(b1.getAuthor());
-	}
-}
+		System.out.println("The Author of the created book is: "+ b0.getAuthor());	
+		System.out.println("The Author of the created book is going to fail cos of int value: "+ b1.getAuthor());
+		
+	}	//End of main method
+}	//End of class TestGetAuthor
--- src/library/book/TestGetAuthorSuccess.java
+++ src/library/book/TestGetAuthorSuccess.java
@@ -11,11 +11,14 @@
 public class TestGetAuthorSuccess {
 	public static void main(String[] args) {	//Start of main method
 		Book b0 = new Book("Nelson", "Murder In Paris", "R123", 111);
+		Book b1 = new Book(1234, "Murder In Paris", "R123", 111);
 		System.out.println("\nTesting the getAuthor() method from the book class");
 		System.out.println("******************************************************");
 		System.out.println("");
+		
 		/** Display the results */
 		System.out.println("The Author of the created book is: "+ b0.getAuthor());	
+		System.out.println("The Author of the created book is going to fail cos of int value: "+ b1.getAuthor());
 		
 	}	//End of main method
 }	//End of class TestGetAuthorSuccess
