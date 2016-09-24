e4668932d29b93834eef3c529d305223bef28cd4
rpreet <rpreet72@gmail.com>
2016-09-24 16:18:15 +1000

    Testing the book class

--- src/library/book/TestBook.java
+++ src/library/book/TestBook.java
@@ -1,15 +1,26 @@
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
 /** Start of the class TestBook */
 public class TestBook {
 	/** Start of the main method */
 	public static void main(String[] args) {
 		/** Create four Book objects */
-		Book b0 = new Book("Raman", "RRR", "CN0", 111);
-		Book b1 = new Book("Gourav", "GGG", "CN1", 222);
-		Book b2 = new Book("Chiran", "CCC", "CN2", 333);
-		Book b3 = new Book("Jino", "JJJ", "CN3", 444);
+		Book b0 = new Book("Nelson", "Murder In Paris", "R111", 111);
+		Book b1 = new Book("David", "Duck on a Bike", "R222", 222);
+		Book b2 = new Book("Potter", "The Tale of Peter Rabbit", "R333", 333);
+		Book b3 = new Book("Kipling", "The Jungle Book", "R444", 444);
 		
 		/** Testing the getAuthor() method from the class Book */
-		System.out.println("\nTesting the getAuthor() method from the book class....");
+		System.out.println("\nTesting the getAuthor() method from the book class.");
+		System.out.println("******************************************************");
+		System.out.println("");
 		String author0 = b0.getAuthor(); 
 		String author1 = b1.getAuthor(); 
 		String author2 = b2.getAuthor(); 
@@ -18,9 +29,13 @@ public class TestBook {
 		/** Display the results */
 		System.out.println("The authors of the created books are:");
 		System.out.println(author0 + ", " + author1 + ", " + author0 + ", " + author0 + ".");
+		System.out.println("=======================================================================");
+		System.out.println("");
 		
 		/** Testing the getTitle() method from the class Book */
-		System.out.println("\nTesting the getTitle() method from the book class....");
+		System.out.println("\nTesting the getTitle() method from the book class.");
+		System.out.println("******************************************************");
+		System.out.println("");
 		String title0 = b0.getTitle(); 
 		String title1 = b1.getTitle(); 
 		String title2 = b2.getTitle(); 
@@ -29,9 +44,13 @@ public class TestBook {
 		/** Display the results */
 		System.out.println("The titles of the created books are:");
 		System.out.println(title0 + ", " + title1 + ", " + title2 + ", " + title3 + ".");
+		System.out.println("=======================================================================");
+		System.out.println("");
 		
 		/** Testing the getCallNumber() method from the class Book */
-		System.out.println("\nTesting the getCallNumber() method from the book class....");
+		System.out.println("\nTesting the getCallNumber() method from the book class.");
+		System.out.println("******************************************************");
+		System.out.println("");
 		String callNumber0 = b0.getCallNumber(); 
 		String callNumber1 = b1.getCallNumber(); 
 		String callNumber2 = b2.getCallNumber(); 
@@ -40,9 +59,13 @@ public class TestBook {
 		/** Display the results */
 		System.out.println("The call numbers of the created books are:");
 		System.out.println(callNumber0 + ", " + callNumber1 + ", " + callNumber2 + ", " + callNumber3 + ".");
+		System.out.println("=======================================================================");
+		System.out.println("");
 		
 		/** Testing the getID() method from the class Book */
-		System.out.println("\nTesting the getID() method from the book class....");
+		System.out.println("\nTesting the getID() method from the book class.");
+		System.out.println("******************************************************");
+		System.out.println("");
 		int id0 = b0.getID(); 
 		int id1 = b1.getID(); 
 		int id2 = b2.getID(); 
@@ -51,14 +74,18 @@ public class TestBook {
 		/** Display the results */
 		System.out.println("The id of the created books are:");
 		System.out.println(id0 + ", " + id1 + ", " + id2 + ", " + id3 + ".");
+		System.out.println("=======================================================================");
+		System.out.println("");
 		
 		/** Testing the toString() method from the class Book */
-		System.out.println("\nTesting the toString() method from the book class....");
+		System.out.println("\nTesting the toString() method from the book class.");
 		/** Display the results */
-		System.out.println("The full details of the created books are:");
+		System.out.println("The description of the created books are:");
 		System.out.println(b0.toString() + "\n"); 
 		System.out.println(b1.toString() + "\n");
 		System.out.println(b2.toString() + "\n");
 		System.out.println(b3.toString() + "\n");
+		System.out.println("=======================================================================");
+		System.out.println("");
 	} // End of the main method 
 } // End of the class TestBook
