4bc5cf1a896c1ce0a0a1760b8a0d110190a994ea
rpreet <rpreet72@gmail.com>
2016-09-24 15:24:14 +1000

    Testing the id of the book

---
+++ src/library/book/TestGetIdSuccess.java
@@ -0,0 +1,23 @@
+/**
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 23.09.2016
+* @version 1.1
+*/
+
+/** Start of TestGetIdSuccess class */
+public class TestGetIdSuccess {
+	public static void main(String[] args) {	//Start of main method
+		Book b0 = new Book("Nelson", "Murder In Paris", "R123", 111);
+		
+		System.out.println("\nTesting the getID() method from the book class");
+		System.out.println("******************************************************");
+		System.out.println("");
+		
+		/** Display the results */
+		System.out.println("The ID of the created book is: " + b0.getID());
+		
+	}	//End of main method
+}	//End of TestGetIdSuccess class
