2bd44b0bb75ef88285db438693084517d55d8cc3
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-26 00:27:02 +1000

    Header comments

--- src/library/BookMapDAOIntegrationTest/TestBookMapDAO.java
+++ src/library/BookMapDAOIntegrationTest/TestBookMapDAO.java
@@ -1,3 +1,11 @@
+/**
+*@reviewer Chiranjivi Bashyal
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy (who has been remarkably helpful)
+*@due date 29.09.2016
+*@version 1.2
+*/
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
