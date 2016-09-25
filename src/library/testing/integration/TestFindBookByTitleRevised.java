ff445d3d10643727df3be9678325974d55674ca9
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-25 21:33:16 +1000

    testing TestFindBookByTitleRevised

---
+++ src/library/testing/integration/TestFindBookByTitleRevised.java
@@ -0,0 +1,37 @@
+package library.testing.integration;
+
+import library.daos.BookHelper;
+import library.daos.BookMapDAO;
+import library.interfaces.daos.IBookDAO;
+
+/**
+*@reviewer Chiranjivi Bashyal
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 23.09.2016
+*@version 2.0
+*
+		
+*/
+public class TestFindBookByTitleRevised {
+
+	public static void main(String[] args) {
+		IBookDAO testBookMapDAO = new BookMapDAO(new BookHelper());
+		
+		
+		//Testing addBook Method
+		testBookMapDAO.addBook("testAuthor", "testTitle", "CN11");
+		System.out.println("Book Added");
+		
+		
+		
+		//Testing find Books Title Method Revised
+		System.out.println("Testing findBook By Title Method Revised. We will give non existent information");
+		System.out.println("Given 'Title' as search string");
+		System.out.println("Output should be blank Array");
+		System.out.println("Output: "+testBookMapDAO.findBooksByTitle("Title"));
+
+	}
+
+}
