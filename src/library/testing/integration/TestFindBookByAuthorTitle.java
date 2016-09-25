a6872524fc73a388144496389f6e3d386e9ba749
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-25 21:31:19 +1000

    testing TestFindBookByAuthorTitle

---
+++ src/library/testing/integration/TestFindBookByAuthorTitle.java
@@ -0,0 +1,37 @@
+package library.testing.integration;
+
+import library.daos.BookHelper;
+import library.daos.BookMapDAO;
+import library.interfaces.daos.IBookDAO;
+
+/**
+*@reviewer Chiranjivi Bashyal
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 23.09.2016
+*@version 2.0
+*
+		
+*/
+public class TestFindBookByAuthorTitle {
+
+	public static void main(String[] args) {
+		IBookDAO testBookMapDAO = new BookMapDAO(new BookHelper());
+		
+		
+		//Testing addBook Method
+		testBookMapDAO.addBook("testAuthor", "testTitle", "CN11");
+		System.out.println("Book Added");
+		
+		
+		
+		//Testing find Books by Author Title Method
+		System.out.println("Testing findBook By Author,Title Method.");
+		System.out.println("Given 'testAuthor', 'testTitle' as search string");
+		System.out.println("Output should be the array with details like 'testAuthor', 'testTitle'");
+		System.out.println("Output: "+testBookMapDAO.findBooksByAuthorTitle("testAuthor","testTitle"));
+
+	}
+
+}
