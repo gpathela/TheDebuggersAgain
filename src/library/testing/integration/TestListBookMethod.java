3b8436148281d4b74eccfff2acc35bf1e62bc541
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-25 21:38:07 +1000

    testing TestListBookMethod

---
+++ src/library/testing/integration/TestListBookMethod.java
@@ -0,0 +1,36 @@
+package library.testing.integration;
+
+import library.daos.BookHelper;
+import library.daos.BookMapDAO;
+import library.interfaces.daos.IBookDAO;
+/**
+*@reviewer Chiranjivi Bashyal
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 23.09.2016
+*@version 2.0
+*
+		
+*/
+
+public class TestListBookMethod {
+
+	public static void main(String[] args) {
+		IBookDAO testBookMapDAO = new BookMapDAO(new BookHelper());
+		
+		
+		//Testing addBook Method
+		testBookMapDAO.addBook("testAuthor", "testTitle", "CN11");
+		System.out.println("Book Added");
+		
+		
+		
+		//Testing List Book Method
+		System.out.println("Testing listBook Method.");
+		System.out.println("Output should be the array with details like 'testAuthor', 'testTitle'");
+		System.out.println("Output: "+testBookMapDAO.listBooks());
+
+	}
+
+}
