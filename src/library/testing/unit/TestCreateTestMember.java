cbc65bcee5154356ed68e2d15d17084601f1ff21
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-09-25 21:24:25 +1000

    testing TestCreateTestMember

---
+++ src/library/testing/unit/TestCreateTestMember.java
@@ -0,0 +1,33 @@
+/** Import some Java libraries and specify the package in which the classes are organized */
+package library.testing.unit;
+
+
+import library.entities.Member;
+
+/**
+*@reviewer Chiranjivi Bashyal
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 23.09.2016
+*@version 2.0
+*
+*		The TestCreateTestMember class is a program that create a Test Member
+*/
+
+/** Start of the class TestCreateTestMember */
+
+public class TestCreateTestMember { 
+
+	public static void main(String[] args) {
+
+		
+		Member testMember = new Member("testFirstName", "testLastName", "0424XXX", "test@test.com", 27); // Creating
+																											// a
+																											// test
+																											// member
+
+		System.out.println("Made a test Member");
+
+	}
+}
